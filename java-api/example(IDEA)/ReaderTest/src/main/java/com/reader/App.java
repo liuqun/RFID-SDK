223b4a963c98293dbdc647c027c25b4762cf239f
刘群 <liuqun@22tianbo.com>
2021-12-01 15:17:16 +0800

    更新测试程序

--- java-api/example(IDEA)/ReaderTest/src/main/java/com/reader/App.java
+++ java-api/example(IDEA)/ReaderTest/src/main/java/com/reader/App.java
@@ -5,16 +5,60 @@ import com.gg.reader.api.dal.GClient;
 import com.gg.reader.api.dal.HandlerTagEpcLog;
 import com.gg.reader.api.dal.HandlerTagEpcOver;
 import com.gg.reader.api.protocol.gx.*;
-
+import javax.usb.UsbConfiguration;
+import javax.usb.UsbDevice;
+import javax.usb.UsbDeviceDescriptor;
+import javax.usb.UsbEndpoint;
+import javax.usb.UsbException;
+import javax.usb.UsbHostManager;
+import javax.usb.UsbHub;
+import javax.usb.UsbInterface;
+import javax.usb.UsbInterfacePolicy;
+import javax.usb.UsbPipe;
+import javax.usb.UsbServices;
+import java.io.UnsupportedEncodingException;
 import java.util.Hashtable;
+import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
 
 public class App {
+    public static Optional<UsbDevice> findUsbDeviceByID(int idVendor, int idProduct) {
+        UsbServices srv;
+        UsbHub hub;
+
+        try {
+            srv = UsbHostManager.getUsbServices();
+            hub = srv.getRootUsbHub();
+            for (Object it: hub.getAttachedUsbDevices()) {
+                UsbDevice item = (UsbDevice) it;
+                UsbDeviceDescriptor desc = item.getUsbDeviceDescriptor();
+                if (desc.idVendor() == idVendor && desc.idProduct() == idProduct) {
+                    System.out.println("调试信息: 已经找到RFID发卡器设备！");
+                    return Optional.of(item);
+                }
+                System.out.printf("调试信息: USB厂商代码VID = 0x%04x\n", desc.idVendor());
+                System.out.printf("调试信息: 产品类型代码PID = 0x%04x\n\n", desc.idProduct());
+                //System.out.println(item.getProductString());
+            }
+        } catch (UsbException ex1) {
+            String reason = "";
+            System.out.println(reason);
+        }
+        return Optional.empty();
+    }
     public static void main(String[] args) {
+        Optional<UsbDevice> usbDevice =
+                findUsbDeviceByID(0x03EB, 0x2421);
+        if (!usbDevice.isPresent()) {
+            System.out.println("请检查读卡器USB数据线，重新插拔一次");
+        }
         GClient client = new GClient();
         Scanner sc = new Scanner(System.in);
-//        if (client.openTcp("192.1.1.168:8160", 0))
-        if (client.openSerial("COM11:115200", 2000)) {
+        if (!usbDevice.isPresent() || !client.openUsbHid(usbDevice.get())) {
+            System.out.println("Connect failure.");
+        }
+        else {
             // 订阅标签上报事件
             client.onTagEpcLog = new HandlerTagEpcLog() {
                 @Override
@@ -43,13 +87,13 @@ public class App {
                 System.out.println("Stop error.");
             }
 
-            // 功率配置, 将4个天线功率都设置为30dBm.
+            // 功率配置, 将所有天线功率都设置为0dBm.（注意:USB桌面发卡器只有一路天线）
             MsgBaseSetPower msgBaseSetPower = new MsgBaseSetPower();
             Hashtable<Integer, Integer> hashtable = new Hashtable<>();
-            hashtable.put(1, 30);
-            hashtable.put(2, 30);
-            hashtable.put(3, 30);
-            hashtable.put(4, 30);
+            hashtable.put(1, 0);
+            //hashtable.put(2, 0);
+            //hashtable.put(3, 0);
+            //hashtable.put(4, 0);
             msgBaseSetPower.setDicPower(hashtable);
             client.sendSynMsg(msgBaseSetPower);
             if (0 == msgBaseSetPower.getRtCode()) {
@@ -64,7 +108,7 @@ public class App {
 
             // 4个天线读卡, 读取EPC数据区以及TID数据区
             MsgBaseInventoryEpc msgBaseInventoryEpc = new MsgBaseInventoryEpc();
-            msgBaseInventoryEpc.setAntennaEnable(EnumG.AntennaNo_1 | EnumG.AntennaNo_2 | EnumG.AntennaNo_3 | EnumG.AntennaNo_4);
+            msgBaseInventoryEpc.setAntennaEnable(EnumG.AntennaNo_1);//msgBaseInventoryEpc.setAntennaEnable(EnumG.AntennaNo_1 | EnumG.AntennaNo_2 | EnumG.AntennaNo_3 | EnumG.AntennaNo_4);
             msgBaseInventoryEpc.setInventoryMode(EnumG.InventoryMode_Inventory);
 
             ParamEpcReadTid tid = new ParamEpcReadTid();
@@ -92,8 +136,6 @@ public class App {
 			System.out.println("Close the connection");
             client.close();
 
-        } else {
-            System.out.println("Connect failure.");
         }
     }
 
